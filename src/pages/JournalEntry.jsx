ced2962ed50c508aedb394b162c9a6421439cf7c
Lynette Cargo <lynettecargo@gmail.com>
2023-09-04 15:32:30 -0500

    updated journalentry

--- src/pages/JournalEntry.jsx
+++ src/pages/JournalEntry.jsx
@@ -1,23 +1,29 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom"; //param passed in url via :id
+import { useAuthContext } from "../hooks/useAuthContext";
 
 // see content of one entry
 const JournalEntry = () => {
     const {id} = useParams()
     const [entry, setEntry] = useState(null)
+    const {user} = useAuthContext()
 
     // fetch single entry
     useEffect(() => {
         const fetchEntry = async() => {
-            const response = await fetch(`http://localhost:4000/api/journal/${id}`)
+            const response = await fetch(`http://localhost:4000/api/journal/${id}`,{
+                headers: {
+                    'Authorization': `Bearer ${user.token}`
+                }
+            })
             const json = await response.json()
 
             if(response.ok){
                 setEntry(json)
             }
         }
-        fetchEntry()
-    }, [])
+        if(user) fetchEntry()
+    }, [user, id])
 
     // no entries - return null
     if(!entry){
