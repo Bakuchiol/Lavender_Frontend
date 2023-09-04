44598da08e68c32bf8ab5a8a2acb0b70a2a0be31
Lynette Cargo <lynettecargo@gmail.com>
2023-09-04 15:16:48 -0500

    router and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import './App.css';
-import { useRoutes } from 'react-router-dom';
+import { useRoutes, Navigate } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
 import Layout from './pages/Layout';
 import Home from './pages/Home'
 import JournalEntry from './pages/JournalEntry';
+import Signup from './pages/Signup'
+import Login from './pages/Login'
 
 const App = () => {
   // youtube, reactrouter docs, stackoverflow
+  const { user } = useAuthContext()
   const elements = useRoutes([
     {path: '/', 
       element: <Layout />, // every time path is / layout is displayed! no wrapping like before
       children: [
-        {path: '/', element: <Home />},
-        {path: '/api/journal/:id', element: <JournalEntry />}
+        // ternary navigates if authenticated or not
+        {path: '/', element: user ? <Home /> : <Navigate to="/api/login" />},
+        {path: '/api/journal/:id', element: user ? <JournalEntry /> : <Navigate to="/api/login" />},
+        {path: '/api/signup', element: !user ? <Signup /> : <Navigate to="/" />},
+        {path: '/api/login', element: !user ? <Login /> : <Navigate to="/" />}
       ]
     } 
   ])
